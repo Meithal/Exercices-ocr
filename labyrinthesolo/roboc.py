372dc9c7c206c68f08f6fe0a42d23a5b51d01376
Meithal <hurin8888@gmail.com>
2018-02-18 00:27:45 +0100

    Display driver

--- labyrinthesolo/roboc.py
+++ labyrinthesolo/roboc.py
@@ -13,42 +13,34 @@ VALID_INPUT_CHARS = 'QNSEO23456789'
 LONGUEUR_MAX_INPUT = 100
 
 
-class Labyrinthe:
-    def __init__(self, texte, nom):
-
-        self.flux = texte
-        self.nom = nom
-
-        self.longueur_colonne = len(self.flux.split("\n")[0].strip())
-
-        for l in self.flux:
-            if l not in LEGAL_CHARS:
-                self.flux = self.flux.replace(l, '')
-
-        self.position_initiale = self.flux.index(CARACTERE_JOUEUR)
-        self.flux = self.flux.replace(CARACTERE_JOUEUR, ' ')
-
-        self.position_joueur = self.position_initiale
-
-    def affiche(self):
-        for (i, c) in enumerate(self.flux):
-            if i == self.position_joueur:
-                print(CARACTERE_JOUEUR, end='')
-            else:
-                print(c, end='')
-            if not (i + 1) % self.longueur_colonne:
-                print()
-        print()
-
-    def save(self):
-        with open(SAVE_FILE, 'w') as f:
-            f.write(self.nom + "\n" + str(self.position_joueur))
-
-
 class Jeu:
 
-    co = None
+    afficher = None
     cartes = []
+    labyrinthe = None
+
+    class Labyrinthe:
+        def __init__(self, texte, nom):
+
+            self.id = len(Jeu.cartes)
+
+            self.flux = texte
+            self.nom = nom
+
+            self.longueur_colonne = len(self.flux.split("\n")[0].strip())
+
+            for l in self.flux:
+                if l not in LEGAL_CHARS:
+                    self.flux = self.flux.replace(l, '')
+
+            self.position_initiale = self.flux.index(CARACTERE_JOUEUR)
+            self.flux = self.flux.replace(CARACTERE_JOUEUR, ' ')
+
+            self.position_joueur = self.position_initiale
+
+        def save(self):
+            with open(SAVE_FILE, 'w') as f:
+                f.write(self.nom + "\n" + str(self.position_joueur))
 
     @staticmethod
     def execute_input(i):
@@ -74,58 +66,81 @@ class Jeu:
 
         for _ in range(combien_de_fois):
             destinations = {
-                'N': Jeu.co.position_joueur - Jeu.co.longueur_colonne,
-                'S': Jeu.co.position_joueur + Jeu.co.longueur_colonne,
-                'E': Jeu.co.position_joueur + 1,
-                'O': Jeu.co.position_joueur - 1
+                'N': Jeu.labyrinthe.position_joueur - Jeu.labyrinthe.longueur_colonne,
+                'S': Jeu.labyrinthe.position_joueur + Jeu.labyrinthe.longueur_colonne,
+                'E': Jeu.labyrinthe.position_joueur + 1,
+                'O': Jeu.labyrinthe.position_joueur - 1
             }
-            if destinations[coord] < 0 or destinations[coord] > len(Jeu.co.flux):
+            if destinations[coord] < 0 or destinations[coord] > len(Jeu.labyrinthe.flux):
                 return True
-            if Jeu.co.flux[destinations[coord]] in BLOCKING_CHARS:
+            if Jeu.labyrinthe.flux[destinations[coord]] in BLOCKING_CHARS:
                 return True
-            elif Jeu.co.flux[destinations[coord]] in VICTORY_CHARS:
+            elif Jeu.labyrinthe.flux[destinations[coord]] in VICTORY_CHARS:
                 print("Vous avez gagné !")
                 os.unlink(SAVE_FILE)
                 return False
             else:
-                Jeu.co.position_joueur = destinations[coord]
+                Jeu.labyrinthe.position_joueur = destinations[coord]
                 if _ < combien_de_fois - 1:
-                    Jeu.co.affiche()
-                Jeu.co.save()
+                    Jeu.afficher()
+                Jeu.labyrinthe.save()
         return True
 
     @staticmethod
-    def commencer():
+    def charger_cartes():
         for f in os.listdir('cartes'):
             with open('cartes/' + f) as carte:
-                Jeu.cartes.append(Labyrinthe(carte.read(), f))
+                Jeu.cartes.append(Jeu.Labyrinthe(carte.read(), f))
 
-        if SAVE_FILE in os.listdir('.'):
-            if input("Voulez-vous continuer la partie en cours ? (O/N)\n> ").strip().upper() == 'O':
-                with open(SAVE_FILE) as f:
-                    try:
-                        nom, place = f.read().split("\n")
-                        
-                        Jeu.co = next(_ for _ in Jeu.cartes if _.nom == nom)
-                        Jeu.co.position_joueur = int(place)
-                    except (IndexError, ValueError):
-                        print("Un problème est survenu en essayant de charger votre sauvegarde")
-                        os.unlink(SAVE_FILE)
-                        Jeu.co = None
-        if Jeu.co is None:
-            print("Veuillez choisir une carte")
-            for _, c in enumerate(Jeu.cartes):
-                print("[{}] {}".format(_ + 1, c.nom.replace('.txt', '')))
-
+    @staticmethod
+    def charger_sauvegarde(fichier):
+        with open(fichier) as f:
             try:
-                Jeu.co = Jeu.cartes[int(input()) - 1]
-            except (ValueError, IndexError):
-                Jeu.co = Jeu.cartes[0]
+                nom, place = f.read().split("\n")
+                Jeu.labyrinthe = next(_ for _ in Jeu.cartes if _.nom == nom)
+                Jeu.labyrinthe.position_joueur = int(place)
+            except (IndexError, ValueError):
+                print("Un problème est survenu en essayant de charger votre sauvegarde.")
+                os.unlink(fichier)
+                Jeu.labyrinthe = None
 
-        Jeu.co.affiche()
+    @staticmethod
+    def commencer():
+        Jeu.afficher()
         while Jeu.execute_input(input("Veuillez entrer une commande (Q: Quitter, N/S/E/O(2-9) : Se diriger\n> ")):
-            Jeu.co.affiche()
+            Jeu.afficher()
+
+
+class AffichageASCII:
+    """Ce driver affiche le jeu en mode ASCII"""
+    @staticmethod
+    def __call__():
+        for (i, c) in enumerate(Jeu.labyrinthe.flux):
+            if i == Jeu.labyrinthe.position_joueur:
+                print(CARACTERE_JOUEUR, end='')
+            else:
+                print(c, end='')
+            if not (i + 1) % Jeu.labyrinthe.longueur_colonne:
+                print()
+        print()
 
 
 if __name__ == "__main__":
+    Jeu.afficher = AffichageASCII()
+    Jeu.charger_cartes()
+    if not len(Jeu.cartes):
+        print("Aucune carte trouvée.")
+        exit()
+    if SAVE_FILE in os.listdir('.'):
+        if input("Voulez-vous continuer la partie en cours ? (O/N)\n> ").strip().upper() == 'O':
+            Jeu.charger_sauvegarde(SAVE_FILE)
+    if Jeu.labyrinthe is None:
+        print("Veuillez choisir une carte")
+        for _ in Jeu.cartes:
+            print("[{}] {}".format(_.id + 1, _.nom.replace('.txt', '')))
+        try:
+            Jeu.labyrinthe = Jeu.cartes[int(input()) - 1]
+        except (ValueError, IndexError):
+            Jeu.labyrinthe = Jeu.cartes[0]
+
     Jeu.commencer()
