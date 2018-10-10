4640da9d1efdd635875017279c5234392416206e
Meithal <hurin8888@gmail.com>
2018-10-10 21:05:14 +0200

    Leave experimental things in branch

--- labyrinthemulti/client.py
+++ labyrinthemulti/client.py
@@ -2,12 +2,7 @@
 # Commande : "py -3 .\serveur.py"
 
 
-import sys
-
-if "pygame" in sys.argv:
-    import jeu.logique_pygame as jeu
-else:
-    import jeu.logique_client as jeu
+import jeu.logique_client as jeu
 
 if __name__ == '__main__':
     jeu.client()
